9ea4ead90a1fff1a4aa134a2c33f3d4b26b19726
Mai Schwartz <mai.schwartz@gmail.com>
2018-01-02 20:50:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
-var newEmployees = ['Ada', 'Brendan', 'Ali'];
 
-function printBadges(newEmployees) {
-  for (i=0, i<newEmployees.length, i++) {
+
+function printBadges(arr) {
+  for (i=0, i<arr.length, i++) {
     console.log("Welcome ${newEmployees[i]}! You are employee #${i}.")
   }
   console.log(newEmployees)
