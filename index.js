9adc6732cd941a8c58c722c2626c77e350c24782
Mai Schwartz <mai.schwartz@gmail.com>
2018-01-02 20:47:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+var newEmployees = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges(newEmployees) {
+  for (i=0, i<newEmployees.length, i++) {
+    console.log("Welcome ${newEmployees[i]}! You are employee #${i}.")
+  }
+  console.log(newEmployees)
+}
